1bf21262021d9ccbd3566a4a1d937c9b8476f4f8
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-03-17 19:20:40 +0900

    commit

--- 06_java_ObjectOrientedPrograming_basic/src/step6_01/classObject/ClassEx12_정답.java
+++ 06_java_ObjectOrientedPrograming_basic/src/step6_01/classObject/ClassEx12_정답.java
@@ -14,6 +14,8 @@ package step6_01.classObject;
  * 
  */
 
+import java.util.Scanner;
+
 /*
 class Ex12{
 	String[][] game = new String[3][3];
@@ -27,7 +29,36 @@ class Ex12{
 public class ClassEx12_정답 {
 	
 	public static void main(String[] args) {
-
+		
+		Scanner sc = new Scanner(System.in);
+		Ex12 e = new Ex12();
+		
+		System.out.println(" ===틱택토===");
+		for (int i = 0; i < e.game.length; i++) {
+			for (int j = 0; j < e.game.length; j++) {
+				System.out.print(" [ ]");
+			}
+			System.out.println();
+		}
+		System.out.println("========================");
+		
+		System.out.print("Player 1 : ");
+		int player1 = sc.nextInt();
+		
+		
+		
+		
+//		while(true) {
+//			
+//			
+//			
+//			
+//			
+//		}
+		
+		
+		
+		
 	}
 
 }
